e7feec2bdf513a2aeae048df75b305ebec1f875e
star07316588 <44515399+star07316588@users.noreply.github.com>
2025-05-23 16:01:40 +0800

    Update CerRegAdd.cs

--- CerRegAdd.cs
+++ CerRegAdd.cs
@@ -1,8 +1,22 @@
+
 [HttpPost]
 public ActionResult Add(CerRegListVM vm)
 {
-    var userId = Session["userid"]?.ToString();
-    var role = Session["role"]?.ToString(); // OpSupervisor-Leader / OpTrainingGroup / Admin
+    vm.StationList = _TestingService.GetStationIdList().ToList();
+    vm.StationOptions = vm.StationList.Select(x => new SelectListItem
+    {
+        Value = x,
+        Text = x
+    });
+
+    vm.CerItemList = _TestingService.GetCerItemIdList(vm.StationId).ToList();
+    vm.CerItemOptions = vm.CerItemList.Select(x => new SelectListItem
+    {
+        Value = x,
+        Text = x,
+    }).ToList();
+
+    var role = deptId; // OpSupervisor-Leader / OpTrainingGroup / Admin
     var monthly = vm.Month;
     var empId = vm.EmpId;
     var cerItemId = vm.CertItemId;
@@ -16,31 +30,31 @@ public ActionResult Add(CerRegListVM vm)
     {
         message = $"考試科目:{cerItemId}, 該月份不在鑑定作業清單內, 新增失敗! (工號:{empId}, 站別:{stationId}, 月份:{monthly})";
         TempData["ErrorMessage"] = message;
-        return RedirectToAction("RegList");
+        return View("Index", vm);
     }
 
-    var cerId = certInfo.CerId;
-    var cerDate = certInfo.CerDate;
+    var cerId = certInfo.Cer_Id;
+    var cerDate = certInfo.Cer_Date;
 
     if (service.IsDuplicateRegistration(cerId, cerDate, empId, cerItemId))
     {
         message = "資料已經存在!";
         TempData["ErrorMessage"] = message;
-        return RedirectToAction("RegList");
+        return View("Index", vm);
     }
 
     if (!service.HasTrainingRecordInThreeMonths(empId, cerItemId))
     {
         message = $"考試科目:{cerItemId}, 三個月內沒有訓練紀錄，若要報名請洽訓練組! ({empId}, {stationId}, {monthly})";
         TempData["ErrorMessage"] = message;
-        return RedirectToAction("RegList");
+        return View("Index", vm);
     }
 
     var examType = service.GetExamType(cerItemId);
     var scoreWR = examType == "OP" ? "NA" : "";
     var scoreOP = examType == "WR" ? "NA" : "";
 
-    service.InsertRegistration(new CertRegisterVM
+    service.InsertRegistration(new CerRegListVM
     {
         CerId = cerId,
         EmpId = empId,
@@ -51,6 +65,6 @@ public ActionResult Add(CerRegListVM vm)
         CreateUser = userId
     });
 
-    TempData["SuccessMessage"] = "資料新增完成!";
-    return RedirectToAction("RegList");
-}
+            TempData["SuccessMessage"] = "資料新增完成!";
+            return View("Index", vm);
+        }
