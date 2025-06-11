517678e25aa180312c090eeeabd371b8e7278437
star07316588 <44515399+star07316588@users.noreply.github.com>
2025-06-11 17:26:27 +0800

    Create ScoreQueryModel.cs

---
+++ ScoreQueryModel.cs
@@ -0,0 +1,17 @@
+namespace MesTAManagementSystem_New.Models.Training.Testing
+{
+    public class ScoreQueryModel
+    {
+        public string StationId { get; set; }
+        public string CerItemId { get; set; }
+        public string WrDate { get; set; }
+        public string OpDate { get; set; }
+        public string ShiftId { get; set; }
+        public string EmpId { get; set; }
+        public string Name { get; set; }
+        public string ScoreWriting { get; set; }
+        public string ScoreOper { get; set; }
+        public string WrFile { get; set; }
+        public string OpFile { get; set; }
+    }
+}
