40f489a65ebfa57720179ff46c604d7756670a29
star07316588 <44515399+star07316588@users.noreply.github.com>
2025-07-02 16:52:51 +0800

    Update ExamServics.cs

--- ExamServics.cs
+++ ExamServics.cs
@@ -30,4 +30,33 @@ public class ExamService : IExamService
     {
         return _examRepository.GetQuestionsByType(cerItemId, "3");
     }
+
+// Service: ExamService.cs
+public class ExamService
+{
+    private readonly ExamRepository _examRepository;
+
+    public ExamService(ExamRepository examRepository)
+    {
+        _examRepository = examRepository;
+    }
+
+    public List<ExamQuestion.QuestionResult> GetRandomTrueFalseAndChoiceQuestions(string cerItemId, int questionCount)
+    {
+        var allQuestions = _examRepository.GetTrueFalseAndChoiceQuestions(cerItemId);
+        return allQuestions.OrderBy(q => Guid.NewGuid()).Take(questionCount).ToList();
+    }
+
+    public List<ExamQuestion.QuestionResult> GetLinkQuestions(string cerItemId, int linkSum, int linkSubSum)
+    {
+        return _examRepository.GetLinkQuestions(cerItemId, linkSum, linkSubSum);
+    }
+
+    public int GetTotalExamTimeInSeconds(string cerItemId)
+    {
+        int minutes = _examRepository.GetExamTimeMinutes(cerItemId);
+        return minutes > 0 ? minutes * 60 : 600;
+    }
+}
+
 }
