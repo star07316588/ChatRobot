4d7dc6e5f267cc97653fe925eb6d92832ffed168
star07316588 <44515399+star07316588@users.noreply.github.com>
2026-04-07 17:36:01 +0800

    Update GeneralPdf.cs

--- GeneralPdf.cs
+++ GeneralPdf.cs
@@ -8,3 +8,14 @@
                         "--footer-font-size 8 " +
                         "--footer-line"
                 };
+
+var pdfView = new ViewAsPdf("CreatePdf", vm)
+{
+    PageSize = Rotativa.Options.Size.A4,
+    CustomSwitches =
+        "--header-right \"General PD\nMacronix Proprietary\" " + // 直接換行
+        "--header-font-size 9 " +
+        "--footer-center \"This document may contain personal data...\" " +
+        "--footer-font-size 8 " +
+        "--footer-line"
+};
