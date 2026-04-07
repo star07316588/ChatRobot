0bdee4a3c8f7d55c1d05316069452a0ac0c4018e
star07316588 <44515399+star07316588@users.noreply.github.com>
2026-04-07 17:32:42 +0800

    Create GeneralPdf.cs

---
+++ GeneralPdf.cs
@@ -0,0 +1,10 @@
+                var pdfView = new ViewAsPdf("CreatePdf", vm)
+                {
+                    PageSize = Rotativa.Options.Size.A4,
+                    CustomSwitches =
+                        "--header-right \"General PD\\nMacronix Proprietary\" " +
+                        "--header-font-size 9 " +
+                        "--footer-center \"This document may contain personal data, and shall be used for MXIC's business only.\" " +
+                        "--footer-font-size 8 " +
+                        "--footer-line"
+                };
