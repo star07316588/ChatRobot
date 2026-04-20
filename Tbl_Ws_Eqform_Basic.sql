12117948fda4cdfde7e10fbb1a6513e0599b408f
star07316588 <44515399+star07316588@users.noreply.github.com>
2026-04-20 17:07:31 +0800

    Update Tbl_Ws_Eqform_Basic.sql

--- Tbl_Ws_Eqform_Basic.sql
+++ Tbl_Ws_Eqform_Basic.sql
@@ -1,4 +1,4 @@
-我在oracle下以下指令查詢顯示資料為0筆，照禮截斷字串1到8結果應該會一樣
+我在oracle下以下指令查詢顯示資料為0筆，照理截斷字串1到8結果應該會一樣
 select * from Tbl_Ws_Eqform_Basic a where substr(a.lotid, 1, 8) = '5P530600A5'
 但下以下指令卻有資料，原因是什麼
 select * from Tbl_Ws_Eqform_Basic a where a.lotid = '5P530600'
